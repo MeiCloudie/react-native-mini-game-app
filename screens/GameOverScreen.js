c75732e980a5588f03a20cda5cd43ba54c36faf6
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-07-18 19:56:33 +0700

    feat: Checking for "Game Over"

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,6 +1,7 @@
+import { Text } from "react-native";
+
 function GameOverScreen() {
-    return <></>;
-  }
-  
-  export default GameOverScreen;
-  
+  return <Text>Game is over!</Text>;
+}
+
+export default GameOverScreen;
